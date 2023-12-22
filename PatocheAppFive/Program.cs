21274f950b6df5061d17ff39d9ad5118a18f361b
Patrice Hanne <patrice.hanne@expleogroup.com>
2023-12-22 14:40:58 +0100

    Ajoutez des fichiers projet.

---
+++ PatocheAppFive/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PatocheAppFive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,this is my new application");
+        }
+    }
+}
